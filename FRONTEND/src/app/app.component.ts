c34034676a7183cbb4a7f00d393133d8bc35d751
Raphael <raphael.silva@primecontrol.com.br>
2023-04-25 12:13:33 -0300

    First Commit

---
+++ FRONTEND/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Tarefa } from './tarefa';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'TODOapp';
+  arrayDeTarefas: Tarefa[] = [];
+  apiURL: string;
+
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000';
+    this.READ_tarefas();
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+
+  DELETE_tarefa(Tarefa_Removida: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(Tarefa_Removida);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); });
+
+  }
+
+  READ_tarefas() {
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+      resultado => this.arrayDeTarefas = resultado);
+  }
+
+  UPDATE_tarefa(Tarefa_Modificada: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(Tarefa_Modificada);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+      Tarefa_Modificada).subscribe(
+        resultado => { console.log(resultado); this.READ_tarefas(); });
+  }
+}
+//1063
